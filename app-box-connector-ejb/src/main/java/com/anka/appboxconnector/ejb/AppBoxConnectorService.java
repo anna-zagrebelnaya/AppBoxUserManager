f36559e4e950258fa2b6bb1d80150958edc7bc87
anna-zagrebelnaya <annzagrebelnaya@gmail.com>
2015-06-19 18:50:28 +0300

    some refactoring of rest + javadocs

--- app-box-connector-ejb/src/main/java/com/anka/appboxconnector/ejb/AppBoxConnectorService.java
+++ app-box-connector-ejb/src/main/java/com/anka/appboxconnector/ejb/AppBoxConnectorService.java
@@ -4,12 +4,39 @@ import com.anka.appboxconnector.model.AppBoxUser;
 
 import javax.ejb.Local;
 
+/**
+ * This service provides access to <a href="https://app.box.com">Box</a> using a developer token
+ *  for authentication into it. Please read <a href="https://developers.box.com/docs/">API docs</a>
+ *  to know how to get a developer token.
+ */
 @Local
 public interface AppBoxConnectorService {
 
+    /**
+     * Returns information about the user for whom specified auth token was generated.
+     *
+     * @param authToken  the auth token was generated on <a href="https://app.box.com">Box</a>
+     * @return retrieved user
+     * @throws com.anka.appboxconnector.exceptions.AppBoxConnectorException if token is invalid
+     */
     AppBoxUser getCurrentUser(String authToken);
 
+    /**
+     * Returns information about the newly created user.
+     *
+     * @param authToken  the auth token was generated on <a href="https://app.box.com">Box</a>
+     * @return newly created user
+     * @throws com.anka.appboxconnector.exceptions.AppBoxConnectorException if the fields are
+     * invalid or token is invalid or token is generated for non-admin account.
+     */
     AppBoxUser createUser(AppBoxUser user, String authToken);
 
+    /**
+     * Deletes a user in an enterprise account.
+     *
+     * @param authToken  the auth token was generated on <a href="https://app.box.com">Box</a>
+     * @throws com.anka.appboxconnector.exceptions.AppBoxConnectorException if token is invalid
+     * or the user still has files in their account.
+     */
     void deleteUser(String id, String authToken);
 }
--- app-box-connector-web/src/main/java/com/anka/appboxconnector/api/rest/services/BoxUsersRestService.java
+++ app-box-connector-web/src/main/java/com/anka/appboxconnector/api/rest/services/BoxUsersRestService.java
@@ -1,14 +1,46 @@
 package com.anka.appboxconnector.api.rest.services;
 
+import com.anka.appboxconnector.ejb.AppBoxConnectorService;
 import com.anka.appboxconnector.model.AppBoxUser;
 
+import javax.ejb.EJB;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-public interface BoxUsersRestService {
+@Path("/users")
+@Produces(MediaType.APPLICATION_JSON + "; charset=UTF-8")
+public class BoxUsersRestService {
+    private static final String ID = "id";
+    @Context
+    private HttpHeaders headers;
 
-    Response getCurrentUser(String authToken);
+    @EJB
+    private AppBoxConnectorService appBoxConnectorService;
 
-    Response createUser(String authToken, AppBoxUser appBoxUser);
+    @GET
+    @Path("/current")
+    public Response getCurrentUser(@HeaderParam("Authorization") String authToken) {
+        AppBoxUser user = appBoxConnectorService.getCurrentUser(authToken);
+        return Response.ok(user).build();
+    }
 
-    Response deleteUser(String authToken, String id);
+    @POST
+    @Path("/")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response createUser(@HeaderParam("Authorization") String authToken,
+                                AppBoxUser appBoxUser) {
+        AppBoxUser user = appBoxConnectorService.createUser(appBoxUser, authToken);
+        return Response.ok(user).build();
+    }
+
+    @DELETE
+    @Path("/{" + ID + "}")
+    public Response deleteUser(@HeaderParam("Authorization") String authToken,
+                               @PathParam(ID) String id) {
+        appBoxConnectorService.deleteUser(id, authToken);
+        return Response.ok("User with id " + id + " is deleted").build();
+    }
 }
